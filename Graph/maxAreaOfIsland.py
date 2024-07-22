6db2d32fb65be293b7a50e1340a9ad9d39fd573b
Akshit <sainiakshit85@gmail.com>
2024-07-22 22:28:47 +0530

    Max Area of Island

--- Graph/maxAreaOfIsland.py
+++ Graph/maxAreaOfIsland.py
@@ -1,1 +1,36 @@
 #Checking Astro vim
+
+class Solution:
+    def maxAreaOfIsland(self,grid):
+        R=len(grid)
+        C=len(grid[0])
+        res=float("-inf")
+
+        def dfs(r,c):
+            if r<0 or c<0 or r>=R or c>=C or grid[r][c]==0:
+                return 0
+            grid[r][c]=0            
+            area=1
+            area+=dfs(r+1,c)
+            area+=dfs(r-1,c)
+            area+=dfs(r,c+1)
+            area+=dfs(r,c-1)
+            return area
+
+
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c]==1:
+                    res=max(res,dfs(r,c))
+        return res
+
+
+grid = [
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0]
+]
+
+sol=Solution()
+print(sol.maxAreaOfIsland(grid))
