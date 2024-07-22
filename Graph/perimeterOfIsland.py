1347aceb5f9b0bec5236866f7ab0d02ab1876fb7
Akshit <sainiakshit85@gmail.com>
2024-07-23 01:17:20 +0530

    added directions array

--- Graph/perimeterOfIsland.py
+++ Graph/perimeterOfIsland.py
@@ -1,29 +1,24 @@
 class Solution:
-    def perimeter(self,grid):
-        #Rows of Grid
+    def parameterOfIsland(self,grid):
         R=len(grid)
-        #Columns of Grid
         C=len(grid[0])
-        
+        directions=[[1,0],[0,1],[-1,0],[0,-1]]
+
         def dfs(r,c):
-            if r<0 or c<0 or r>=R or c>=C or grid[r][c]==0:
+            if r<0 or c<0 or r>=R or c>=C or  grid[r][c]==0:
                 return 1
-            # if we get a visited island
             if grid[r][c]==-1:
                 return 0
-            # Isalnd that we have visited
             grid[r][c]=-1
-            perimeter=dfs(r+1,c)
-            perimeter+=dfs(r,c+1)
-            perimeter+=dfs(r-1,c)
-            perimeter+=dfs(r,c-1)
-            return perimeter
-        
+            peri=0
+            for dr, dc in directions:
+                peri+=dfs(r+dr,c+dc)
+            return peri
         for r in range(R):
             for c in range(C):
                 if grid[r][c]==1:
                     return dfs(r,c)
-                
+
 grid = [
     [0, 1, 0, 0],
     [1, 1, 1, 0],
@@ -32,6 +27,4 @@ grid = [
 ]
 
 sol=Solution()
-print(sol.perimeter(grid=grid)) 
-
-#checking lazygit
+print(sol.parameterOfIsland(grid))
