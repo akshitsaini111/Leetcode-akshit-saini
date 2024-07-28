3a74403951026d2fafb3d7c27cb72db770de77a8
Akshit <sainiakshit85@gmail.com>
2024-07-28 16:24:06 +0530

    code to run the function added
    Return statement also added

--- LinkedList/reorderLL.py
+++ LinkedList/reorderLL.py
@@ -1,4 +1,4 @@
-class node:
+class Node:
 
     def __init__(self, val, next=None):
         self.val = val
@@ -36,3 +36,36 @@ class Solution:
 
             first = temp1
             second = temp2
+
+        return head
+
+
+# Helper functions to print and create the linked list
+def print_list(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+
+def create_list(values):
+    if not values:
+        return None
+    head = Node(values[0])
+    current = head
+    for value in values[1:]:
+        current.next = Node(value)
+        current = current.next
+    return head
+
+
+# Example usage
+values = [1, 2, 3, 4, 5]
+head = create_list(values)
+print("Original list:")
+print_list(head)
+
+solution = Solution()
+reordered_head = solution.reOrder(head)
+print("Reordered list:")
+print_list(reordered_head)
