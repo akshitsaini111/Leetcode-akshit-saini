d4aa405f9a52cb02f20c691e9aafdc4b7afde696
Akshit <sainiakshit85@gmail.com>
2024-07-28 22:34:35 +0530

    Copy Linked LIst With Random Pointer

--- LinkedList/copyLinkedListWithRandomPointer.py
+++ LinkedList/copyLinkedListWithRandomPointer.py
@@ -1,7 +1,25 @@
 class node:
-    def __init__(self,val,next=None):
-        self.val=val
-        self.next=next
+
+    def __init__(self, val, next=None, random=None):
+        self.val = val
+        self.next = next
+        self.random = random
+
 
 class Solution:
-    def copyRandomPointer(self,head):
+
+    def copyRandomPointer(self, head):
+        copy = {None: None}
+        curr = head
+        while curr:
+            temp = curr.val
+            copy[curr] = node(temp)
+            curr = curr.next
+
+        curr = head
+        while curr:
+            node = copy[curr]
+            node.next = copy[curr.next]
+            node.random = copy[curr.random]
+            curr = curr.next
+        return copy[head]
