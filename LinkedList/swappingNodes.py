48402bc7448fd603cedf0d0777c34c8b0554c878
Akshit <sainiakshit85@gmail.com>
2024-07-28 21:15:34 +0530

    Swap node from Start and End
    You are give a integer n you have to swap n node from start and n node
    from the end

--- LinkedList/swappingNodes.py
+++ LinkedList/swappingNodes.py
@@ -1,7 +1,27 @@
 class node:
-    def __init__(self,val,next=None):
-        self.val=val
-        self.next=next
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
 
 class Solution:
-    def swapNodes(self,head,n):
+
+    def swapNodes(self, head, n):
+        left = head
+        for _ in range(n - 1):
+            left = left.next
+
+        curr = head
+        length = 1
+        while curr.next:
+            curr = curr.next
+            length += 1
+
+        right = head
+        for _ in range(length - n):
+            right = right.next
+
+        left.val, right.val = right.val, left.val
+
+        return head
