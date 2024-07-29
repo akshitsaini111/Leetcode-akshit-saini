c8d2dbb2211af12010b69ff9574b07533f1a712c
Akshit <sainiakshit85@gmail.com>
2024-07-29 16:09:17 +0530

    Kth larget Element

--- Heaps/kThLargestElement.py
+++ Heaps/kThLargestElement.py
@@ -1,2 +1,11 @@
+import heapq
+
+
 class Solution:
-    def KthLarget(self,nums):
+
+    def KthLarget(self, nums, k):
+        nums = [-i for i in nums]
+        heapq.heapify(nums)
+        for _ in range(k):
+            res = heapq.heappop(nums)
+        return abs(res)
