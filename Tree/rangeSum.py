3875477efcfe81822c98429bf6aa084ec58ac759
Akshit <sainiakshit85@gmail.com>
2024-08-01 23:01:17 +0530

    Range Sum in Tree

--- Tree/rangeSum.py
+++ Tree/rangeSum.py
@@ -4,3 +4,20 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
+
+
+class Solution:
+
+    def rangeSum(self, root, low, high):
+        res = [0]
+
+        def dfs(node):
+            if not node:
+                return
+            if low <= node.val <= high:
+                res[0] += node.val
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return res[0]
