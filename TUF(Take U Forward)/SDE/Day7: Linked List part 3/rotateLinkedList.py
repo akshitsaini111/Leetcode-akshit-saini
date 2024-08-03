1c29363c3f35417e002dc346eb1b9c53ae53b6d1
Akshit <sainiakshit85@gmail.com>
2024-08-03 19:53:16 +0530

    Rotate Linked List

--- TUF(Take U Forward)/SDE/Day7: Linked List part 3/rotateLinkedList.py
+++ TUF(Take U Forward)/SDE/Day7: Linked List part 3/rotateLinkedList.py
@@ -1,8 +1,26 @@
-
 class node:
 
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
+
+    def rotate(self, head, k):
+        tail = head
+        length = 1
+        while tail.next:
+            tail = tail.next
+            length += 1
+        k = k % length
+        if k == 0:
+            return head
+        curr = head
+        for _ in range(length - k - 1):
+            curr = curr.next
+
+        new_head = curr.next
+        curr.next = None
+        tail.next = head
+        return new_head
