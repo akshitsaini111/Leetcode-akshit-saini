e61634b78446304b672ef0a74c2fed7ba02b4e99
Akshit <sainiakshit85@gmail.com>
2024-08-03 19:59:33 +0530

    Copy A Linked List With A Random Pointer

---
+++ TUF(Take U Forward)/SDE/Day7: Linked List part 3/copyListWithRandomPointer.py
@@ -0,0 +1,25 @@
+class node:
+
+    def __init__(self, val, next=None, random=None):
+        self.val = val
+        self.next = next
+        self.random = random
+
+
+class Solution:
+
+    def copy(self, head):
+        copy = {None: None}
+        curr = head
+        while curr:
+            temp = curr.val
+            copy[curr] = node(temp)
+            curr = curr.next
+        curr = head
+        while curr:
+            node = copy[curr]
+            node.next = copy[curr.next]
+            node.random = copy[curr.random]
+
+            curr = curr.next
+        return copy[head]
