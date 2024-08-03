6586c33baf027aa8cc9312f44a902648d3664ed8
Akshit <sainiakshit85@gmail.com>
2024-08-03 12:43:45 +0530

    Reverse Linked List In K Groups

--- TUF(Take U Forward)/SDE/Day6: Linked List Part-2/reverseNodeInKGroups.py
+++ TUF(Take U Forward)/SDE/Day6: Linked List Part-2/reverseNodeInKGroups.py
@@ -8,4 +8,26 @@ class node:
 class Solution:
 
     def revKGroup(self, head, k):
-        return
+        dummy = node(0, head)
+        gp = dummy
+        while True:
+            kth = self.getKth(head, k)
+            if not kth:
+                break
+            gn = kth.next
+            prev = kth.next
+            curr = gp.next
+            while curr != gn:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            gp.next, gp = kth, gp.next
+
+        return dummy.next
+
+    def getKth(self, head, k):
+        while head and k > 0:
+            head = head.next
+            k -= 1
+        return head
