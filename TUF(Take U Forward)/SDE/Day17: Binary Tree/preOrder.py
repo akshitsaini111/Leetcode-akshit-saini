cf0e549159f79f62dd7a2e753c835de5ce899331
Akshit <sainiakshit85@gmail.com>
2024-08-03 20:17:37 +0530

    PreOrder Traversal

---
+++ TUF(Take U Forward)/SDE/Day17: Binary Tree/preOrder.py
@@ -0,0 +1,21 @@
+class Node:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def preorder(self, root):
+        res = []
+
+        def dfs(node):
+            if node:
+                res.append(node.val)
+                dfs(node.left)
+                dfs(node.right)
+
+        dfs(root)
+        return res
