5aaed4576931cf5e33ad39bf73ef113eb4d9530c
Akshit <sainiakshit85@gmail.com>
2024-08-03 21:13:35 +0530

    Left View Of Tree

---
+++ TUF(Take U Forward)/SDE/Day17: Binary Tree/leftView.py
@@ -0,0 +1,10 @@
+
+class Node:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def leftView(self,root):
