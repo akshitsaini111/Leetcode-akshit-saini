f91636518a8d8716113d09df655b6f130ca764be
Akshit <sainiakshit85@gmail.com>
2024-08-04 11:26:26 +0530

    Level Order Traversal Of A Tree

--- TUF(Take U Forward)/SDE/Day18: Binary Tree Part2/levelOrderTraversal.py
+++ TUF(Take U Forward)/SDE/Day18: Binary Tree Part2/levelOrderTraversal.py
@@ -7,3 +7,24 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
+
+
+class Solution:
+
+    def levelOrder(self, root):
+        q = deque()
+        q.append(root)
+        res = []
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            if level:
+                res.append(level)
+        return res
