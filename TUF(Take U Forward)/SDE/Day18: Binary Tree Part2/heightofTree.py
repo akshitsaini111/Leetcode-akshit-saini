c7653fcefd78614efb40d750dcfa617089885151
Akshit <sainiakshit85@gmail.com>
2024-08-04 11:54:11 +0530

    Max Depth Of Tree

--- TUF(Take U Forward)/SDE/Day18: Binary Tree Part2/heightofTree.py
+++ TUF(Take U Forward)/SDE/Day18: Binary Tree Part2/heightofTree.py
@@ -4,3 +4,11 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
+
+
+class Solution:
+
+    def height(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.height(root.left), self.height(root.right))
