d3aa2931528617a54913a6b2d5b2f36cd76da2c9
Akshit <sainiakshit85@gmail.com>
2024-08-05 01:26:39 +0530

    Insert In Binary Tree

---
+++ Tree/insertInTree.py
@@ -0,0 +1,18 @@
+class node:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def insert(self, root, val):
+        if not root:
+            return
+        if val > root.val:
+            root.right = self.insert(root.right, val)
+        if val < root.val:
+            root.left = self.insert(root.left, val)
+        return root
