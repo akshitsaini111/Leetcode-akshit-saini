18196cced99e4d0ce4774acdc17f99b2b804e466
Akshit <sainiakshit85@gmail.com>
2024-08-07 21:55:55 +0530

    Climbin stairs Bottom Up Approach

---
+++ Dynamic Programming/climbingStairs.py
@@ -0,0 +1,11 @@
+# Bottom UP -Approach
+class Solution:
+
+    def noOfWays(self, n):
+        one = 1
+        two = 1
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
