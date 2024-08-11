632c414a109f0b0f9d288cec16935b51e5720150
Akshit <sainiakshit85@gmail.com>
2024-08-11 20:58:07 +0530

    Cycle Detection in Directed Acyclic Graph

--- TUF(Take U Forward)/A-Z/Step-15/Lec:3 TopoLogical Sort/cyclicDetectionDAG.py
+++ TUF(Take U Forward)/A-Z/Step-15/Lec:3 TopoLogical Sort/cyclicDetectionDAG.py
@@ -1,3 +1,23 @@
 class Solution:
-    def cycleDetection(self,v,adj):
 
+    def cycleDetection(self, v, adj):
+        visiting = set()
+        visited = set()
+
+        def dfs(j):
+            if j in visiting:
+                return True
+            if j in visited:
+                return False
+            visiting.add(j)
+            for nei in adj[j]:
+                if dfs(j):
+                    return True
+            visited.add(j)
+            visiting.remove(j)
+            return False
+
+        for i in range(v):
+            if dfs(i):
+                return True
+        return False
