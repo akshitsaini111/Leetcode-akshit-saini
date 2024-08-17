bfafc55829bbd73b01e67dce9e81fdcecf6d2f1b
Akshit <sainiakshit85@gmail.com>
2024-08-18 01:32:27 +0530

    Delete Node In A Binary Search Tree

--- Love Babbar 450/Binary Search Tree/deleteNode.py
+++ Love Babbar 450/Binary Search Tree/deleteNode.py
@@ -11,3 +11,19 @@ class Solution:
     def deletNode(self, root, key):
         if not root:
             return
+        if key > root.val:
+            root.right = self.deletNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deletNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                curr = root.right
+                while curr.left:
+                    curr = curr.left
+                root.val = curr.val
+                root.right = self.deletNode(root.right, curr.val)
+        return root
