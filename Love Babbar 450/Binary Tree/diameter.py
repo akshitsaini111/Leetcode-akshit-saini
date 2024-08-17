7e75bf7b6b5a14a92a1fb1d9ac35d406543120aa
Akshit <sainiakshit85@gmail.com>
2024-08-17 19:55:53 +0530

    Diameter Of A Tree

---
+++ Love Babbar 450/Binary Tree/diameter.py
@@ -0,0 +1,24 @@
+class Node:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def diameter(self, root):
+        res = [0]
+
+        def dfs(node):
+            if not node:
+                return 0
+            l = dfs(node.left)
+            r = dfs(node.right)
+            d = l + r
+            res[0] = max(d, res[0])
+            return 1 + max(l, r)
+
+        dfs(root)
+        return res[0]
