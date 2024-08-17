ca957ff10ef4b09b6a1afa163a13d9ffea96c295
Akshit <sainiakshit85@gmail.com>
2024-08-18 01:29:58 +0530

    Delete Node In A Binary Search Tree

---
+++ Love Babbar 450/Binary Search Tree/deleteNode.py
@@ -0,0 +1,13 @@
+class Node:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def deletNode(self, root, key):
+        if not root:
+            return
