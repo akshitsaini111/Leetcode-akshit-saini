1d880d72c296706ca5d8e05f797016ac20192cb3
Akshit <sainiakshit85@gmail.com>
2024-08-18 19:52:37 +0530

    Longest Increasing SubSequence

---
+++ Dynamic Programming/longestIncreasingSubSequence.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def longestIncreasingSubSequence(self, nums):
+        LIS = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+
+        return max(LIS)
