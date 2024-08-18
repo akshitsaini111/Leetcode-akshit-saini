07d989830fedf3d85953aa6973a05a16d67ff2ee
Akshit <sainiakshit85@gmail.com>
2024-08-18 18:42:24 +0530

    Max Product SubArray

--- Dynamic Programming/maxProductSubArray.py
+++ Dynamic Programming/maxProductSubArray.py
@@ -1,11 +1,12 @@
 class Solution:
-    def maxProductSubArray(self,nums):
-        res=max(nums)
-        currMax=1
-        currMin=1
-        for n in nums;
-            temp=currMax*n
-            currMax=max(temp,currMin*n,n)
-            currMin=min(temo,currMin*n,n)
-            res=max(res,currMax)
+
+    def maxProductSubArray(self, nums):
+        res = max(nums)
+        currMax = 1
+        currMin = 1
+        for n in nums:
+            temp = currMax * n
+            currMax = max(temp, currMin * n, n)
+            currMin = min(temp, currMin * n, n)
+            res = max(res, currMax)
         return res
