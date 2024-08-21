c4c9c79f357369cedaeccd07b5352466c3bd1b7c
Akshit <sainiakshit85@gmail.com>
2024-08-22 00:47:49 +0530

    Permutation In A String

---
+++ Sliding Window/permutationInString.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def permInString(self, s1, s2):
+        l = 0
+        for r in range(len(s2)):
+            if (r - l + 1) == len(s1):
+                if sorted(s1) == sorted(s2[l : r + 1]):
+                    return True
+            else:
+                l += 1
+        return False
