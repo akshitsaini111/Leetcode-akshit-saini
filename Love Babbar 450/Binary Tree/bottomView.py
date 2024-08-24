925d00917dcfb0f9d1f8a647f2fea8c40fa1b1df
Akshit <sainiakshit85@gmail.com>
2024-08-24 20:35:00 +0530

    Bottom View Completed

--- Love Babbar 450/Binary Tree/bottomView.py
+++ Love Babbar 450/Binary Tree/bottomView.py
@@ -13,3 +13,16 @@ class Solution:
 
     def bottomView(self, root):
         q = deque()
+        q.append([root, 0])
+        bv = {}
+        while q:
+            node, hd = q.popleft()
+            bv[hd] = node
+            if node.left:
+                q.append([node.left, hd - 1])
+            if node.right:
+                q.append([node.right, hd + 1])
+
+        sk = sorted(bv.keys())
+        res = [bv[hd].val for hd in sk]
+        return res
